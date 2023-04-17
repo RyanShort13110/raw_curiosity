f797e11191f189f5160e0bace36a3e6abbf8121e
Ryan Short <ryanshort.dev@gmail.com>
2023-04-17 00:01:51 -0400

    large style update

--- js/main.js
+++ js/main.js
@@ -18,21 +18,20 @@ More advanced things to add...
 - search all images by id
 */
 
-document.querySelector('.make-fetch').addEventListener('click', getFetch)
-document.querySelector('.toggle-view').addEventListener('click', toggleView)
+document.querySelector(".make-fetch").addEventListener("click", getFetch)
+document.querySelector(".toggle-view").addEventListener("click", toggleView)
 
-// Needless to say, I made fetch happen
-function getFetch(){
-  document.querySelector('ul').textContent = '' // clear on search
-  document.querySelector('.show-error').textContent = '' // clear
-  document.querySelector('.cam-type').textContent = '' // clear
-  const date = document.querySelector('input').value // get date from input
+function getFetch() {
+  document.querySelector("ul").textContent = "" // clear these on new search
+  document.querySelector(".rover-name").textContent = ""
+  document.querySelector(".cam-type").textContent = ""
+  const date = document.querySelector("input").value // get date
 
   // changing api query parameters using DOM elements
-  const camChoice = document.getElementById("camera") // grab options from select
-  const roverCam = camChoice.options[camChoice.selectedIndex].value
-  const roverChoice = document.getElementById('rover')
-  const userRover = roverChoice.options[roverChoice.selectedIndex].value
+  const camChoice = document.getElementById("camera") // get "options" from the "select" elements
+  const roverChoice = document.getElementById("rover")
+  const roverCam = camChoice.options[camChoice.selectedIndex].value // get camera option value
+  const userRover = roverChoice.options[roverChoice.selectedIndex].value // get rover option value
 
   // use these to check if correct value is being obtained
   // console.log(userRover)
@@ -41,38 +40,44 @@ function getFetch(){
   const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/${userRover}/photos?earth_date=${date}&camera=${roverCam}&page=1&api_key=637bWjPJ74JSOXq2SpTNqvOldsEceDSnYGNOILfX`
 
   fetch(url)
-      .then(res => res.json())
-      .then(data => {
-        console.log(data)
-        let roverName = data.photos[0].rover.name
-        let camName = data.photos[0].camera['full_name']
-        
-        document.querySelector('.cam-type').textContent = camName
-        document.querySelector('.show-error').textContent = roverName
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data)
+      let roverName = data.photos[0].rover.name
+      let camName = data.photos[0].camera["full_name"]
 
-        // the forEach creates and adds images and their ids to the dom as "li" elements
-        data.photos.forEach((el, i) => {
-          let marsPics = el['img_src']
-          let liElm = document.createElement('li')
-          let roverPic = document.createElement('img')
-          let liText = document.createElement('span')
-          let id = el.id
-          
-          roverPic.src = marsPics
-          liText.textContent = `Image ID: ${id}`
+      document.querySelector(".cam-type").textContent = camName
+      document.querySelector(".rover-name").textContent = roverName
 
-          liElm.appendChild(liText)
-          liElm.appendChild(roverPic)
-          document.querySelector('ul').appendChild(liElm)
-        })
+      // the forEach creates and adds images and their ids to the dom as "li" elements
+      data.photos.forEach((el, i) => {
+        let marsPics = el["img_src"]
+        let liElm = document.createElement("li")
+        let roverPic = document.createElement("img")
+        let liText = document.createElement("span")
+        let id = el.id
 
+        roverPic.src = marsPics
+        liText.textContent = `Image ID: ${id}`
+
+        liElm.appendChild(liText)
+        liElm.appendChild(roverPic)
+        document.querySelector("ul").appendChild(liElm)
       })
-      .catch(err => {
-          console.log(`error ${err}`)
-          document.querySelector('.show-error').textContent = 'No data available. Try a different date or camera.'
-      });
+
+      // only show the "toggle view" button when there are li elements on the page
+      if(document.querySelector('.cam-type').textContent != ''){
+        document.querySelector('.toggle-view').classList.toggle('hidden')
+      }
+
+    })
+    .catch((err) => {
+      console.log(`error: ${err}`)
+      document.querySelector(".rover-name").textContent =
+        "No data available. Try a different date or camera."
+    })
 }
 
-function toggleView(){
-  document.querySelector('ul').classList.toggle('change-view')
+function toggleView() {
+  document.querySelector("ul").classList.toggle("change-view")
 }
